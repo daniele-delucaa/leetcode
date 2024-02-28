9410f2839d955bbc1b98ddd41eb79c295d670eb6
Daniele <daniele.2002.ddl@gmail.com>
2024-02-28 10:26:42 +0100

    fine esercizio, uso visita inorder

--- trees/sameTree.go
+++ trees/sameTree.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -7,25 +9,32 @@ type TreeNode struct {
 }
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-
+	var vis1, vis2 []int
+	inorder(p, &vis1)
+	inorder(q, &vis2)
+	fmt.Println(vis1, vis2)
+	if len(vis1) != len(vis2) {
+		return false
+	}
+	for i := range vis1 {
+		if vis1[i] != vis2[i] {
+			return false
+		}
+	}
+	return true
 }
 
-func dfs(r *TreeNode) []*TreeNode {
-	visited := []*TreeNode{}
+func inorder(r *TreeNode, visited *[]int) {
 	if r == nil {
-		return visited
+		return
 	}
-	return recurse(r, visited)
-}
-
-func recurse(r *TreeNode, visited []*TreeNode) []*TreeNode {
-	visited = append(visited, r)
-
-	if r.Left != nil {
-		visited = recurse(r.Left, visited)
+	if r.Left == nil {
+		*visited = append(*visited, 0)
 	}
-	if r.Right != nil {
-		visited = recurse(r.Right, visited)
+	if r.Right == nil {
+		*visited = append(*visited, 0)
 	}
-	return visited
+	inorder(r.Left, visited)
+	*visited = append(*visited, r.Val)
+	inorder(r.Right, visited)
 }
