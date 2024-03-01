82eef04295a00a77715b328764478adc559b5e11
Daniele <daniele.2002.ddl@gmail.com>
2024-03-01 20:07:51 +0100

    fix

--- topInterview150/maximumDepthOfBinaryTree.go
+++ topInterview150/maximumDepthOfBinaryTree.go
@@ -6,11 +6,12 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-// soluzione ricorsiva
 // un'altro approccio poteva essere usare una BFS e una variabile booleana
 // ogni volta che si aggiungono dei nodi alla coda settiamo a true
 // la variabile, quando è true incrementiamo il contatore e
 // risettiamo a false la var booleana
+
+// soluzione ricorsiva
 func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
