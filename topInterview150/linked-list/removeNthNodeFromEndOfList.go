c51f5c60d77f1772749244b656ccdb9fe621a00e
Daniele <daniele.2002.ddl@gmail.com>
2024-03-03 21:14:01 +0100

    soluzione migliore removeNthNodeFromEndOfList

--- topInterview150/linked-list/removeNthNodeFromEndOfList.go
+++ topInterview150/linked-list/removeNthNodeFromEndOfList.go
@@ -5,25 +5,13 @@ type ListNode struct {
 	Next *ListNode
 }
 
-// soluzione macchinosa, troppi if
+// soluzione decente, sono autistico
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	var listLength int
 	curr := head
 	for curr != nil {
 		listLength++
 		curr = curr.Next
-	}
-	if listLength == 1 {
-		head = nil
-		return head
-	}
-	if listLength == 2 && n == 1 {
-		head.Next = nil
-		return head
-	}
-	if listLength == 2 && n == 2 {
-		head = head.Next
-		return head
 	}
 	var counter int
 	index := listLength - n
@@ -41,4 +29,28 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 		curr = curr.Next
 	}
 	return head
+}
+
+// soluzione con due indici l e r, serve un nodo "dummy", che
+// inizializziamo in testa alla lista (prima di head), facciamo partire
+// i due indici l e r rispettivamente da dummy e head.
+// il primo ciclo serve a far arrivare l'indice r in un nodo.
+// dopo facciamo scorrere contemporaneamente l e r e appena
+// r raggiunge la fine della lista (nil), l avrà raggiunto il
+// nodo prima del nodo da cancellare, e cancelliamo il nodo dopo.
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0, head}
+	l, r := dummy, head
+
+	for i := 0; i < n; i++ {
+		r = r.Next
+	}
+
+	for r != nil {
+		l = l.Next
+		r = r.Next
+	}
+	// cancellazione nodo
+	l.Next = l.Next.Next
+	return dummy.Next
 }
