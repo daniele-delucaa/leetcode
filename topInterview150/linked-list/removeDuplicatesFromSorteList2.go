0c23e35e706315cd8c0f57ed7f6b9eef18e18a0e
Daniele <daniele.2002.ddl@gmail.com>
2024-03-04 14:49:00 +0100

    inizio removeDuplicatesFromSorteList2

---
+++ topInterview150/linked-list/removeDuplicatesFromSorteList2.go
@@ -0,0 +1,12 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+}
