a749e83a3eedabd6a9629e08bdce34a8564898d7
Daniele <daniele.2002.ddl@gmail.com>
2024-03-06 19:06:11 +0100

    soluzione migliore removeDuplicatesFromSortedArray

--- topInterview150/array-string/removeDuplicatesFromSortedArray.go
+++ topInterview150/array-string/removeDuplicatesFromSortedArray.go
@@ -4,20 +4,37 @@ import "sort"
 
 // pongo le occorrenze delle variabili in una mappa
 // pongo le variabili in un'array di supporto
-// ordino l'array di supporto, e gli elementi dell'array di
-// supporto li metto in quello principale
+// ordino l'array di supporto, e riassegno gli elementi
+// dell'array principali con gli elementi dell'array di supporto
 func removeDuplicates(nums []int) int {
-	m := make(map[int]int)
+	m := make(map[int]bool)
 	var supp []int
 	for i := range nums {
-		m[nums[i]]++
-	}
-	for k := range m {
-		supp = append(supp, k)
+		if ok := m[nums[i]]; !ok {
+			supp = append(supp, nums[i])
+		}
+		m[nums[i]] = true
 	}
 	sort.Ints(supp)
 	for i := range supp {
 		nums[i] = supp[i]
 	}
 	return len(supp)
+}
+
+// abbiamo due indici l e r, tutti e due puntano sul secondo
+// elemento dell'array all'inizio. L' indice r avanza a ogni
+// iterazione, l avanza quando troviamo i nuovi elementi.
+// ad ogni iterazione controlliamo se l'elemento puntato da r e
+// il suo predecessore sono diversi, se lo sono poniamo l'elemento
+// di indice r in l. (la fa da "segnaposto")
+func removeDuplicates(nums []int) int {
+	l := 1
+	for r := 1; r < len(nums); r++ {
+		if nums[r] != nums[r-1] {
+			nums[l] = nums[r]
+			l++
+		}
+	}
+	return l
 }
