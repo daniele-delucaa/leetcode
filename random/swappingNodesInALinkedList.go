046e33c6cb62315b23b5c0b523a1d299f20308ad
Daniele <daniele.2002.ddl@gmail.com>
2024-03-06 15:06:57 +0100

    swappingNodesInALinkedList con soluzione migliore

--- random/swappingNodesInALinkedList.go
+++ random/swappingNodesInALinkedList.go
@@ -24,3 +24,28 @@ func swapNodes(head *ListNode, k int) *ListNode {
 	}
 	return head
 }
+
+// soluzione con i due indici, senza l'array di supp
+// iteriamo col for iniziale fino all'arrivo del primo nodo
+// in posizione k e salviamo un puntatore a quel nodo.
+// inizializziamo un altro puntatore che alla fine dovrà
+// puntare in posizione k a partire dalla fine della lista
+// con il secondo for facciamo avanzare i due indici,
+// (curr parte dalla posizione k, r dalla head) appena curr
+// raggiunge la fine della lista, r sarà nella posizione
+// k a partire dalla fine della lista, infine esegui lo swap
+// grazie ai due puntatori
+func swapNodes(head *ListNode, k int) *ListNode {
+	curr := head
+	for i := 0; i < k-1; i++ {
+		curr = curr.Next
+	}
+	l := curr // puntatore al nodo in posizione k da swappare
+	r := head
+	for curr.Next != nil {
+		r = r.Next
+		curr = curr.Next
+	}
+	l.Val, r.Val = r.Val, l.Val
+	return head
+}
