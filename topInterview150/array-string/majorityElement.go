4d0064a4212784ddd2e7edc065f6f1bcd875c946
Daniele <daniele.2002.ddl@gmail.com>
2024-03-06 17:29:47 +0100

    update

--- topInterview150/array-string/majorityElement.go
+++ topInterview150/array-string/majorityElement.go
@@ -1,13 +1,28 @@
 package main
 
 func majorityElement(nums []int) int {
-	var res int
 	occ := make(map[int]int)
 	q := len(nums) / 2
 	for i := range nums {
 		occ[nums[i]]++
 		if occ[nums[i]] > q {
+			return nums[i]
+		}
+	}
+	return 0
+}
+
+// soluzione con algoritmo di boyer moore
+func majorityElement(nums []int) int {
+	var res, count int
+	for i := range nums {
+		if count == 0 {
 			res = nums[i]
+		}
+		if nums[i] == res {
+			count++
+		} else {
+			count--
 		}
 	}
 	return res
