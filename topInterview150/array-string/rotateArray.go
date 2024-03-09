cb270c964916422fe4218518c305a1ebfce44b73
Daniele <daniele.2002.ddl@gmail.com>
2024-03-09 20:28:52 +0100

    rotateArray done

--- topInterview150/array-string/rotateArray.go
+++ topInterview150/array-string/rotateArray.go
@@ -1,23 +1,18 @@
 package main
 
-// non funziona, da rifare
 func rotate(nums []int, k int) {
-	var vals []int
-	for i := range nums {
-		if i >= len(nums)-k {
-			vals = append(vals, nums[i])
-		}
-	}
-	resIndex := len(nums) - 1
-	for i := len(nums) - 1; i >= 0; i-- {
-		if i < len(nums)-k {
-			nums[resIndex] = nums[i]
-			resIndex--
-		}
-	}
-	for i := range vals {
-		if i < k {
-			nums[i] = vals[i]
-		}
+	k = k % len(nums) // we need to do this reassignment to handle the cases where k is greater or equal than the length of the array
+	// if k is equal than the length of the array than the array doesn't need to rotate k times (the array remain the same after the rotations)
+	// if k is greater than the length, we do the module, we are ensuring that k is within range [0, n)
+	reverse(nums)
+	reverse(nums[:k])
+	reverse(nums[k:])
+}
+
+func reverse(arr []int) {
+	j := len(arr) - 1
+	for i := 0; i < j; i++ {
+		arr[i], arr[j] = arr[j], arr[i]
+		j--
 	}
 }
