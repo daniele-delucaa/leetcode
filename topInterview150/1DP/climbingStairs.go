2ea8ef85b5f638b475ad8dfe5fdb39e505eaa305
Daniele <daniele.2002.ddl@gmail.com>
2024-03-10 01:13:09 +0100

    climbingStairs done

--- topInterview150/1DP/climbingStairs.go
+++ topInterview150/1DP/climbingStairs.go
@@ -1,5 +1,16 @@
 package main
 
 func climbStairs(n int) int {
-
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	var supp []int
+	supp = append(supp, 1, 2)
+	for i := 2; i <= n; i++ {
+		supp = append(supp, supp[i-1]+supp[i-2])
+	}
+	return supp[n-1]
 }
