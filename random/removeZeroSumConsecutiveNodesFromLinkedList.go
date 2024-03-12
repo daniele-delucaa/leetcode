5ca52579990d4b82ab1decee9ecffd4dfb81f8f9
Daniele <daniele.2002.ddl@gmail.com>
2024-03-12 17:06:03 +0100

    update removeZeroSumConsecutiveNodesFromLinkedList

--- random/removeZeroSumConsecutiveNodesFromLinkedList.go
+++ random/removeZeroSumConsecutiveNodesFromLinkedList.go
@@ -6,20 +6,27 @@ type ListNode struct {
 }
 
 // prefix sum approach
+// we store all the increasing sum (the prefix sum of a node)
+// in a map, we associate all the sums with the corresponding
+// ListNode. After we re-iterate through the
+// list starting from the dummy node, and we find the next value
+// through the map.
 func removeZeroSumSublists(head *ListNode) *ListNode {
+	// initialization
 	m := make(map[int]*ListNode) // key = prefixSum, value = corresponding node
 	dummy := &ListNode{0, head}
 	m[0] = dummy
-
 	sum := 0
 	curr := head
 
+	// store the sums
 	for curr != nil {
 		sum += curr.Val
 		m[sum] = curr
 		curr = curr.Next
 	}
 
+	// "re-linking" of the LinkedList
 	sum = 0
 	curr = dummy
 	for curr != nil {
