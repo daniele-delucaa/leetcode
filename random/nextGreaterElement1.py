1c8cc6943fed6600c993e0e564d40cfa073598a9
daniele de luca <daniele.2002.ddl@gmail.com>
2024-12-29 00:59:51 +0100

    nextGreaterElement1 monotonic stack solution

--- random/nextGreaterElement1.py
+++ random/nextGreaterElement1.py
@@ -1,3 +1,5 @@
+# Brute force solution
+# O(n * m) n = len(nums1), m = len(nums2)
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         res = []
@@ -13,4 +15,25 @@ class Solution:
                     res.append(-1)
                 index += 1
         return res
-                
+# Monotonic stack solution, using a hashmap where we save the
+# value (as key) and indexes (as value) of an array. Iterate from
+# nums2 and add n in stack if n is in nums1. If n is greater than
+# the element on top of the stack, we found the next greater element
+# of the element in the stack.
+# So we pop from stack and get the index from hashmap and write the 
+# result. 
+# O(n + m)
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = [-1] * len(nums1)
+        stack = []
+        occ = defaultdict(int)
+        for x in nums1:
+            occ[x] = nums1.index(x)
+        for i, n in enumerate(nums2):
+            while len(stack) > 0 and n > stack[len(stack)-1]:
+                v = stack.pop()
+                index = occ[v]
+                res[index] = n
+            if n in occ:
+                stack.append(n)
+        return res
