9c979d425fda29fa41e0e22d3cfbd5b61fab82de
daniele de luca <daniele.2002.ddl@gmail.com>
2025-01-02 23:59:19 +0100

    longestConsecutiveSequence done

--- neetcode-roadmap/array-hashing/longestConsecutiveSequence.py
+++ neetcode-roadmap/array-hashing/longestConsecutiveSequence.py
@@ -1,16 +1,15 @@
-# Time limit excedeed
 # convert nums in hash set, if n - 1 is not in set, n is 
-# the start of a subsequence, looping through and build 
-# the subsequence 
+# the start of a subsequence, looping through (while n + 1 is in set) 
+# and build the subsequence 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        num_set = set(nums)
+        nums = set(nums)
         longest = 0 
 
         for n in nums:
-            if n - 1 not in num_set:
+            if n - 1 not in nums:
                 length = 1
-                while n + length in num_set:   
+                while n + length in nums:   
                     length += 1
                 longest = max(length, longest)
 
